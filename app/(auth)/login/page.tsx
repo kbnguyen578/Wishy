0b0b9bd4e141763f7108a52e9515080f37a216ed
kbnguyen578 <kbnguyen578@gmail.com>
2026-06-23 22:33:59 -0500

    set up the login page and UI with shadcn blocks

---
+++ app/(auth)/login/page.tsx
@@ -0,0 +1,19 @@
+// shadcn Block Code for login page 
+
+import { GalleryVerticalEnd } from "lucide-react"
+import { LoginForm } from "@/app/(auth)/login/components/LoginForm"
+export default function LoginPage() {
+  return (
+    <div className="flex min-h-svh flex-col items-center justify-center gap-6 bg-muted p-6 md:p-10">
+      <div className="flex w-full max-w-sm flex-col gap-6">
+        <a href="#" className="flex items-center gap-2 self-center font-medium">
+          <div className="flex size-6 items-center justify-center rounded-md bg-primary text-primary-foreground">
+            <GalleryVerticalEnd className="size-4" />
+          </div>
+          Acme Inc.
+        </a>
+        <LoginForm />
+      </div>
+    </div>
+  )
+}
