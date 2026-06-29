ca4b0bb5544eb949f5fc27a8d87aca003bea4222
kbnguyen578 <kbnguyen578@gmail.com>
2026-06-29 00:34:29 -0500

    added the logo to the sign in and login page

--- app/(auth)/login/components/LoginForm.tsx
+++ app/(auth)/login/components/LoginForm.tsx
@@ -30,7 +30,7 @@ export function LoginForm({
         <CardHeader className="text-center">
           <CardTitle className="text-xl">Welcome back</CardTitle>
           <CardDescription>
-            Login with your Apple or Google account
+            Login with your Google account
           </CardDescription>
         </CardHeader>
         <CardContent>
--- app/signup/page.tsx
+++ app/signup/page.tsx
@@ -7,10 +7,11 @@ export default function SignupPage() {
     <div className="flex min-h-svh flex-col items-center justify-center gap-6 bg-muted p-6 md:p-10">
       <div className="flex w-full max-w-sm flex-col gap-6">
         <a href="#" className="flex items-center gap-2 self-center font-medium">
-          <div className="flex size-6 items-center justify-center rounded-md bg-primary text-primary-foreground">
-            <GalleryVerticalEnd className="size-4" />
-          </div>
-          Acme Inc.
+          <img 
+            src="/Wishy_Logo.svg"
+            alt="Wishy Logo"
+            className="h-10 w-auto"
+            />
         </a>
         <SignupForm />
       </div>
