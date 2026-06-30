8b7fe1f9361f4bd2b19e632ec0865aa52dc2284b
kbnguyen578 <kbnguyen578@gmail.com>
2026-06-29 21:57:38 -0500

    google sign in redirected issue resolved

--- lib/auth-actions.ts
+++ lib/auth-actions.ts
@@ -69,9 +69,11 @@ export async function signout() {
 
 export async function signInWithGoogle() {
   const supabase = await createClient();
+
   const { data, error } = await supabase.auth.signInWithOAuth({
     provider: "google",
     options: {
+      redirectTo: `${process.env.NEXT_PUBLIC_SITE_URL}/auth/callback`,
       queryParams: {
         access_type: "offline",
         prompt: "consent",
